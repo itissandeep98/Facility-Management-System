7c6af71651e74bb63dd4bf1d3e3316f1c13ca671
Sandeep <itissandeep98@gmail.com>
2020-04-23 15:15:41 +0530

    fxml updated with icons

--- FMS/src/sample/Main.java
+++ FMS/src/sample/Main.java
@@ -32,7 +32,7 @@ public class Main extends Application {
 		public void start(Stage primaryStage) throws Exception {
 				stage = primaryStage;
 				root = FXMLLoader.load(getClass().getResource("WelcomeScreen.fxml"));
-				Image icon = new Image("sample/icon.png");
+				Image icon = new Image("sample/icons/icon.png");
 				primaryStage.getIcons().add(icon);
 				primaryStage.setTitle("Facility  Management System");
 				primaryStage.setScene(new Scene(root));
